cd63640d5887c3d33307993c927567737f250b2f
Kalyanasundaram <kkalyan7@LAMU02CCMDKMD6M.uhc.com>
2021-07-16 15:18:42 -0500

    Added PriorAuthAgentModel

---
+++ PriorAuthAgentModel/src/main/java/model/MedNecAgent.java
@@ -0,0 +1,32 @@
+package model;
+
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+import jade.core.Agent;
+import jade.core.behaviours.OneShotBehaviour;
+
+public class MedNecAgent extends Agent implements DecisionAgent {
+		
+	
+	
+	protected void setup() {
+		System.out.println("MedNecAgent start");
+    	// Try receiving message
+    	addBehaviour(new Messaging());
+	}
+	
+	private class Messaging extends OneShotBehaviour {
+		
+		KieServices ks = KieServices.Factory.get();
+	    KieContainer kContainer = ks.getKieClasspathContainer();
+    	KieSession kSession = kContainer.newKieSession("ksession-mednec");
+		
+		public void action() {
+	    	kSession.insert(myAgent);
+	    	kSession.fireAllRules();
+		}
+	}
+}
