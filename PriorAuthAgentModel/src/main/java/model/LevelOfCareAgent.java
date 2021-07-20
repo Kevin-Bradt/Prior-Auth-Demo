2fb9cbf96043cd5a1605e08c2cd11f520230013f
Ryan Harvey <ryan.harvey@optum.com>
2021-07-20 11:02:03 -0500

    adds level of care agent

---
+++ PriorAuthAgentModel/src/main/java/model/LevelOfCareAgent.java
@@ -0,0 +1,36 @@
+package model;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+import jade.core.Agent;
+import jade.core.behaviours.OneShotBehaviour;
+
+public class LevelOfCareAgent extends Agent implements DecisionAgent {
+	
+	protected void setup() {
+		System.out.println("LevelOfCareAgent start");
+		KieServices ks = KieServices.Factory.get();
+	    KieContainer kContainer = ks.getKieClasspathContainer();
+    	KieSession kSession = kContainer.newKieSession("ksession-levelofcare");
+    	
+    	// Try receiving message
+    	addBehaviour(new Messaging(kSession));
+	}
+	
+	private class Messaging extends OneShotBehaviour {
+		
+		KieSession kSession;
+		
+		public Messaging(KieSession k) {
+			kSession = k;
+		}
+		
+		public void action() {
+	    	kSession.insert(myAgent);
+	    	kSession.fireAllRules();
+		}
+	}
+
+}
