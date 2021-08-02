7a2b67ea28ce57a721c1b6ae15501bf9404c78fa
Ryan Harvey <ryan.harvey@optum.com>
2021-08-02 15:57:09 -0500

    adds data reasoning to med nec to determine approval or denial

--- PriorAuthAgentModel/src/main/java/model/MedNecAgent.java
+++ PriorAuthAgentModel/src/main/java/model/MedNecAgent.java
@@ -4,6 +4,7 @@ package model;
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.FactHandle;
 
 import jade.core.AID;
 import jade.core.Agent;
@@ -12,7 +13,10 @@ import jade.lang.acl.ACLMessage;
 
 public class MedNecAgent extends Agent implements DecisionAgent {
 		
-	private AID levelofcare;
+	private AID levelofcare, service;
+	private Integer esi_level;
+	private String cpt, icd10;
+	
 	
 	public AID getLevelofcare() {
 		return levelofcare;
@@ -20,19 +24,58 @@ public class MedNecAgent extends Agent implements DecisionAgent {
 
 	public void setLevelofcare(AID levelofcare) {
 		this.levelofcare = levelofcare;
+	} 
+
+	public AID getService() {
+		return service;
+	}
+
+	public void setService(AID service) {
+		this.service = service;
+	}
+
+	public Integer getEsi_level() {
+		return esi_level;
+	}
+
+	public void setEsi_level(Integer esi_level) {
+		this.esi_level = esi_level;
+		System.out.println("We made it "+this.esi_level);
+	}
+
+	public String getCpt() {
+		return cpt;
+	}
+
+	public void setCpt(String cpt) {
+		this.cpt = cpt;
+	}
+
+	public String getIcd10() {
+		return icd10;
+	}
+
+	public void setIcd10(String icd10) {
+		this.icd10 = icd10;
 	}
 
 	protected void setup() {
 		System.out.println("MedNecAgent start");
 		KieServices ks = KieServices.Factory.get();
 	    KieContainer kContainer = ks.getKieClasspathContainer();
-    	KieSession kSession = kContainer.newKieSession("ksession-mednec");
+	    KieSession kSession = kContainer.newKieSession("ksession-mednec");
     	
     	// Register the eligibility agent in the yellow pages
     	registerAgent(this, getAID(), "mednec");
     	
     	// Try receiving message
     	addBehaviour(new Messaging(kSession, this));
+	}
+	
+	public void parseContent(String str) {
+		this.cpt = str.split("-")[0];
+		this.icd10 = str.split("-")[1];
+		quickMessage(getLevelofcare(), this, "report esi level", "esi-request");
 	}
 	
 	private class Messaging extends CyclicBehaviour {
@@ -50,6 +93,10 @@ public class MedNecAgent extends Agent implements DecisionAgent {
 	    	//Find level of care
 	    	setLevelofcare(findAgent(myAgent, "levelofcare"));
 	    	System.out.println("Med Nec Found "+getLevelofcare());
+	    	
+	    	//Find level of care
+	    	setService(findAgent(myAgent, "service"));
+	    	System.out.println("Med Nec Found "+getService());
 		}
 		
 		public void action() {
